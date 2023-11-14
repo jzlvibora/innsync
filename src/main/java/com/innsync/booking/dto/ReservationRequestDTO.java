0dffd24fd0766ae5da2018ac33f094b2b6a3e60f
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2023-11-14 23:52:08 +0800

    DTOs

---
+++ src/main/java/com/innsync/booking/dto/ReservationRequestDTO.java
@@ -0,0 +1,44 @@
+package com.innsync.booking.dto;
+
+import java.util.Date;
+
+public class ReservationRequestDTO {
+    private Long roomId;
+    private Date checkInDate;
+    private Date checkOutDate;
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+
+}
---
+++ src/main/java/com/innsync/booking/dto/RoomDTO.java
@@ -0,0 +1,58 @@
+package com.innsync.booking.dto;
+
+public class RoomDTO {
+    private Long roomId;
+    private String roomNumber;
+    private String type;
+    private double price;
+    private int capacity;
+    private String description;
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
